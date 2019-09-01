b3e8513ad5a76f43532e366af888fde0b2ce45c3
J-Fi <y.neczek@gmail.com>
2019-09-01 12:29:35 +0200

    Submodule 22.4: clean TrelloController from System.out.println() and add return phrase

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -20,26 +20,8 @@ public class TrelloController {
     private TrelloClient trelloClient;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTrelloBoards")
-    public void getTrelloBoards() {
-
-        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
-
-        //trelloBoards.forEach(trelloBoardDto -> System.out.println(trelloBoardDto.getId() + " " + trelloBoardDto.getName()));
-/*        trelloBoards.stream()
-                .filter(n -> n.getId() != null && n.getName() != null && n.getName().contains("Kodilla"))
-                .forEach(n -> System.out.println(n.getId() + " " + n.getName()));
-        }*/
-
-        trelloBoards.forEach(trelloBoardDto -> {
-
-            System.out.println(trelloBoardDto.getName() + " - " + trelloBoardDto.getId());
-
-            System.out.println("This board contains lists: ");
-
-            trelloBoardDto.getLists().forEach(trelloList ->
-                    System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed()));
-
-        });
+    public List<TrelloBoardDto> getTrelloBoards() {
+        return trelloClient.getTrelloBoards();
     }
 
     @RequestMapping(method = RequestMethod.POST, value = "createTrelloCard")
