15f1a9ed915e8df4c4599a6251c0677353cf486f
J-Fi <y.neczek@gmail.com>
2019-09-01 13:21:01 +0200

    Submodule 22.4: prepaare //Given section in TrelloClientTest

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -1,13 +1,20 @@
 package com.crud.tasks.trello.client;
 
+import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.trello.config.TrelloConfig;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class TrelloClientTest {
@@ -20,4 +27,23 @@ public class TrelloClientTest {
 
     @Mock
     private TrelloConfig trelloConfig;
+
+    @Test
+    public void shouldFetchTrelloBoards() throws URISyntaxException {
+        //Given
+        when(trelloConfig.getTrelloApiEndpoint()).thenReturn("http://test.com");
+        when(trelloConfig.getTrelloAppKey()).thenReturn("test");
+        when(trelloConfig.getTrelloToken()).thenReturn("test");
+        when(trelloConfig.getTrelloUsername()).thenReturn("janfi3");
+
+        TrelloBoardDto[] trelloBoards = new TrelloBoardDto[1];
+        trelloBoards[0] = new TrelloBoardDto("test_id", "test_board", new ArrayList<>());
+
+        URI uri = new URI("http://test.com/members/janfi3/boards?key=test&token=test&fields=name,id&lists=all");
+
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(trelloBoards);
+        //When
+
+        //Then
+    }
 }
