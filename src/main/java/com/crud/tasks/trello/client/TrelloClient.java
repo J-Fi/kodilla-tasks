199d95147db060933e98e5f84a24cdf9913f76a3
J-Fi <y.neczek@gmail.com>
2019-09-01 12:10:44 +0200

    Submodule 22.4: modify TrelloClient to include LOGGER

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -3,9 +3,12 @@ package com.crud.tasks.trello.client;
 import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -15,8 +18,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+import static org.springframework.data.mapping.Alias.ofNullable;
+
 @Component
 public class TrelloClient {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TrelloClient.class);
 
     @Value("${trello.api.endpoint.prod}")
     private String trelloApiEndpoint;
@@ -34,8 +42,14 @@ public class TrelloClient {
     private RestTemplate restTemplate;
 
     public List<TrelloBoardDto> getTrelloBoards() {
-        TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getUrl(), TrelloBoardDto[].class);
-        return Optional.of(Arrays.asList(boardsResponse)).orElse(new ArrayList<>());
+        try {
+            TrelloBoardDto[] boardsResponse = restTemplate.getForObject(getUrl(), TrelloBoardDto[].class);
+            return Arrays.asList(Optional.ofNullable(boardsResponse).orElse(new TrelloBoardDto[0]));
+        } catch (RestClientException e){
+            LOGGER.error(e.getMessage(), e);
+            return new ArrayList<>();
+        }
+        //Optional.of(Arrays.asList(boardsResponse)).orElse(new ArrayList<>());
     }
 
     private URI getUrl() {
