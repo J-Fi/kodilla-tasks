bde03dc7970e902e4446bde276e4f439a61f53f5
J-Fi <y.neczek@gmail.com>
2019-09-01 21:33:45 +0200

    Submodule 22.4 excercise: add test method shouldReturnEmptyList

--- src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
+++ src/test/java/com/crud/tasks/trello/client/TrelloClientTest.java
@@ -84,4 +84,15 @@ public class TrelloClientTest {
         assertEquals("Test task", newCard.getName());
         assertEquals("http://test.com", newCard.getShortUrl());
     }
+
+    @Test
+    public void shouldReturnEmptyList() throws URISyntaxException {
+        //Given
+        URI uri = new URI("http://test.com/members/janfi3/boards?key=test&token=test&fields=name,id&lists=all");
+        when(restTemplate.getForObject(uri, TrelloBoardDto[].class)).thenReturn(null);
+        //When
+        List<TrelloBoardDto> returnedEmptyList = trelloClient.getTrelloBoards();
+        //Then
+        assertEquals(0, returnedEmptyList.size());
+    }
 }
