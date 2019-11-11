7be813f6a1ce498c8a49253710613a3da724c772
J-Fi <y.neczek@gmail.com>
2019-11-11 23:50:23 +0100

    MOD32.2 add module code

--- src/main/java/com/crud/tasks/service/SimpleEmailService.java
+++ src/main/java/com/crud/tasks/service/SimpleEmailService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -17,9 +19,12 @@ public class SimpleEmailService {
     private static final Logger LOGGER = LoggerFactory.getLogger(SimpleEmailService.class);
 
     @Autowired
+    private MailCreatorService mailCreatorService;
+
+    @Autowired
     private JavaMailSender javaMailSender;
 
-    public void send(final Mail mail) {
+/*    public void send(final Mail mail) {
 
         LOGGER.info("Starting email preparation...");
         try {
@@ -28,15 +33,41 @@ public class SimpleEmailService {
         } catch(MailException e) {
             LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
         }
+    }*/
 
+    public void send(final Mail mail) {
+
+        LOGGER.info("Starting email preparation...");
+        try {
+            javaMailSender.send(createMimeMessage(mail));
+            LOGGER.info("Email has been sent...");
+        } catch(MailException e) {
+            LOGGER.error("Failed to process email sending: ", e.getMessage(), e);
+        }
     }
-
-    private SimpleMailMessage createMailMessage(final Mail mail) {
+    /*private SimpleMailMessage createMailMessage(final Mail mail) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         Optional.ofNullable(mail.getToCc()).ifPresent(cc -> mailMessage.setCc(cc));
         mailMessage.setSubject(mail.getSubject());
         mailMessage.setText(mail.getMessage());
+        return mailMessage;
+    }*/
+
+    private MimeMessagePreparator createMimeMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+    }
+
+    private SimpleMailMessage createMailMessage(final Mail mail) {
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()));
         return mailMessage;
     }
 }
--- src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -6,9 +6,13 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
 
+import javax.mail.internet.MimeMessage;
 import java.util.Optional;
 
 import static org.mockito.Mockito.times;
@@ -23,13 +27,16 @@ public class SimpleEmailServiceTest {
     @Mock
     private JavaMailSender javaMailSender;
 
+    @Mock
+    private MailCreatorService mailCreatorService;
+
     @Test
     public void shouldSendEmail() {
         //Given
-        Mail mail = new Mail("test@test.com", "test2@test2.com", "Test", "Test message");
-        Mail mail2 = new Mail("test@test.com", null, "Test", "Test message");
+        Mail mail = new Mail("test@test.com", "Test", "Test message");
+        Mail mail2 = new Mail("test@test.com", "Test", "Test message");
 
-        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        /*SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setTo(mail.getMailTo());
         Optional.ofNullable(mail.getToCc()).ifPresent(cc -> mailMessage.setCc(cc));
         mailMessage.setSubject(mail.getSubject());
@@ -39,14 +46,27 @@ public class SimpleEmailServiceTest {
         mailMessage2.setTo(mail2.getMailTo());
         Optional.ofNullable(mail2.getToCc()).ifPresent(cc -> mailMessage2.setCc(cc));
         mailMessage2.setSubject(mail2.getSubject());
-        mailMessage2.setText(mail2.getMessage());
+        mailMessage2.setText(mail2.getMessage());*/
 
+        MimeMessagePreparator mimeMessage1 = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail.getMailTo());
+            messageHelper.setSubject(mail.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
+        };
+
+        MimeMessagePreparator mimeMessage2 = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setTo(mail2.getMailTo());
+            messageHelper.setSubject(mail2.getSubject());
+            messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail2.getMessage()), true);
+        };
         //When
         simpleEmailService.send(mail);
         simpleEmailService.send(mail2);
 
         //Then
-        verify(javaMailSender, times(1)).send(mailMessage);
-        verify(javaMailSender, times(1)).send(mailMessage2);
+        verify(javaMailSender, times(1)).send(mimeMessage1);
+        verify(javaMailSender, times(1)).send(mimeMessage2);
     }
 }
