f3f138c4674f8f6d1c6174034b02f19c014f967f
J-Fi <y.neczek@gmail.com>
2019-11-13 21:06:05 +0100

    MOD32.2 Zadanie: ustawienie pozostałych wartosci - dokoÅnczenie

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,6 +1,7 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,9 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     public static final String GOODBYE_MESSAGE = "Thank you for using our website!";
+
+    @Autowired
+    private CompanyConfig companyConfig;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -26,6 +30,10 @@ public class MailCreatorService {
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye_message", GOODBYE_MESSAGE);
+        context.setVariable("company_name", companyConfig.getCompanyName());
+        context.setVariable("company_goal", companyConfig.getCompanyGoal());
+        context.setVariable("company_email", companyConfig.getCompanyEmail());
+        context.setVariable("company_phone", companyConfig.getCompanyPhone());
         return templateEngine.process("/mail/created-trello-card-mail", context);
     }
 }
