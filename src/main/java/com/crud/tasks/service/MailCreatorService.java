033ca486dd610bc78b6e15d3482ef58d92d024d3
J-Fi <y.neczek@gmail.com>
2019-11-13 19:41:11 +0100

    MOD32.2_excercise Zadanie: ustawienie pozostałych wartosci

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -9,6 +10,11 @@ import org.thymeleaf.context.Context;
 @Service
 public class MailCreatorService {
 
+    public static final String GOODBYE_MESSAGE = "Thank you for using our website!";
+
+    @Autowired
+    private AdminConfig adminConfig;
+
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
@@ -16,6 +22,10 @@ public class MailCreatorService {
     public String buildTrelloCardEmail (String message) {
         Context context = new Context();
         context.setVariable("message", message);
-        return templateEngine.process("mail/created-trello-card-mail", context);
+        context.setVariable("tasks_url", "https://j-fi.github.io/");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("goodbye_message", GOODBYE_MESSAGE);
+        return templateEngine.process("/mail/created-trello-card-mail", context);
     }
 }
